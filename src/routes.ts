2e0712c5ea5295c391c3c903ddb116f6bfdd2357
Tiago P Maggioni <t_maggioni@hotmail.com>
2020-08-04 19:29:42 -0300

    controllers

--- src/routes.ts
+++ src/routes.ts
@@ -1,67 +1,17 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/Classes.controller';
+import ConnectionsController from './controllers/ConnectionsController';
+
 
 const routes = express.Router();
 
-interface ScheduleItem {
-    week_day: number;
-    from: string;
-    to: string;
-}
+const classesControllers = new ClassesController();
+const connectionController = new ConnectionsController();
 
-routes.post('/classes', async (request,response) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body;
+routes.post('/classes', classesControllers.create);
+routes.get('/classes', classesControllers.index);
 
-
-    const trx = await db.transaction();
-    
-    try{
-        const insertedUsersIds = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio
-        })
-        const user_id = insertedUsersIds[0];
-    
-        const insertedClassesIds = await trx('classes').insert({
-            subject,
-            cost,
-            user_id
-        });
-    
-        const class_id = insertedClassesIds[0];
-    
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) =>{
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHourToMinutes(scheduleItem.from),
-                to: convertHourToMinutes(scheduleItem.to),
-            }
-        })
-    
-        await trx('class_schedule').insert(classSchedule);
-    
-        await trx.commit();
-    
-        return response.status(201).send();
-    }catch(err){
-        await trx.rollback();
-        return response.status(400).json({
-            error:'Algo errado aconteceu'
-        })
-    }
-    
-})
+routes.get('/connections', connectionController.index);
+routes.post('/connections', connectionController.create);
 
 export default routes;
